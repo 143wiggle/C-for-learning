95e5bccc08537a1f63b9f849268dffc5cd330577
brent just brent <belmontebrent@gmail.com>
2025-04-17 19:45:08 +0800

    3b-comparison-operators.c

---
+++ basic_concepts/3b-comparison-operators.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int x = 5;
+    int y = 10;
+
+    printf("x == y: %d\n", x == y);
+    printf("x != y: %d\n", x != y);
+    printf("x > y: %d\n", x > y);
+    printf("x < y: %d\n", x < y);
+    printf("x >= y: %d\n", x >= y);
+    printf("x <= y: %d\n", x <= y);
+
+    return 0;
+}
+//purpose:
+//Teaches: ==, !=, >, <, >=, <=
+//Note: results are 0 for false, 1 for true
