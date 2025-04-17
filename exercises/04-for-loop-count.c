85fe69e91236c029d942cd04048c155abb8df104
brent just brent <belmontebrent@gmail.com>
2025-04-17 20:42:50 +0800

    04-for-loop-count.c

--- exercises/04-for-loop-count.c
+++ exercises/04-for-loop-count.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    for (int i = 0; i < 5; i++) {
-        printf("Loop iteration: %d\n", i);
+    int iterations;
+
+    // Brief explanation of iterations
+    printf("Iterations: repeating a task using a loop.\n\n");
+
+    // Ask the user how many iterations to loop
+    printf("How many iterations would you like to do? ");
+    scanf("%d", &iterations);
+
+    // Loop through the number of iterations provided by the user
+    printf("\nStarting the loop...\n");
+
+    for (int i = 0; i < iterations; i++) {
+        printf("You are doing iteration: %d\n", i);
     }
+
+    printf("\nLoop complete. You've done %d iterations.\n", iterations);
 
     return 0;
 }
