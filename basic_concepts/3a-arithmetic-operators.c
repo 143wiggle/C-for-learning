f2d9f819b0c223c34d70c6c5ebe871b59145f832
brent just brent <belmontebrent@gmail.com>
2025-04-17 19:44:06 +0800

    3a-arithmetic-operators.c

---
+++ basic_concepts/3a-arithmetic-operators.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = 3;
+
+    printf("Addition: %d + %d = %d\n", a, b, a + b);
+    printf("Subtraction: %d - %d = %d\n", a, b, a - b);
+    printf("Multiplication: %d * %d = %d\n", a, b, a * b);
+    printf("Division: %d / %d = %d\n", a, b, a / b);
+    printf("Modulus: %d %% %d = %d\n", a, b, a % b);
+
+    return 0;
+}
+//Purpose:
+//Teaches: +, -, *, /, %
