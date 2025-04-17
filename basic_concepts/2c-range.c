540a15cf0c5691011a0498468f0cadc40b31f03d
brent just brent <belmontebrent@gmail.com>
2025-04-17 17:58:32 +0800

    2c-range.c

---
+++ basic_concepts/2c-range.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int start = 3;
+    int end = 7;
+
+    for (int i = start; i <= end; i++) {
+        printf("%d ", i);
+    }
+
+    printf("\n");
+    return 0;
+}
